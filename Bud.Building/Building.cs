027ec70384c85f3abe2b34b0e7f7adcd79bb4e2d
Matej Urbas <matej.urbas@gmail.com>
2016-11-06 10:37:25 +0000

    Wrapped some long lines in Building.cs.

--- Bud.Building/Building.cs
+++ Bud.Building/Building.cs
@@ -6,88 +6,114 @@ using System.IO;
 using System.Linq;
 
 namespace Bud {
-  /// <summary>
-  ///   Provides static functions for defining and executing builds.
-  /// </summary>
-  public static class Building {
-    private const int ReservedTimeStringLength = 7;
-
     /// <summary>
-    ///   Executes the build described by the build tasks.
+    ///   Provides static functions for defining and executing builds.
     /// </summary>
-    /// <param name="stdout">the writer to which to print all the build output.</param>
-    /// <param name="buildTasks">the tasks that describe the build.</param>
-    public static void RunBuild(TextWriter stdout, params BuildTask[] buildTasks) {
-      var buildStopwatch = new Stopwatch();
-      buildStopwatch.Start();
-      int totalTasks = CountTasks(buildTasks);
-      var taskNumberAssigner = new TaskNumberAssigner(totalTasks);
-      var builtTaskGraphs = buildTasks.Select(buildTask => ToTaskGraph(stdout, buildTask, taskNumberAssigner, buildStopwatch));
-      var rootTaskGraph = new TaskGraph(builtTaskGraphs);
-      rootTaskGraph.Run();
+    public static class Building {
+        private const int ReservedTimeStringLength = 7;
+
+        /// <summary>
+        ///   Executes the build described by the build tasks.
+        /// </summary>
+        /// <param name="stdout">the writer to which to print all the build output.</param>
+        /// <param name="buildTasks">the tasks that describe the build.</param>
+        public static void RunBuild(TextWriter stdout, params BuildTask[] buildTasks) {
+            var buildStopwatch = new Stopwatch();
+            buildStopwatch.Start();
+            var taskNumberAssigner = new TaskNumberAssigner(CountTasks(buildTasks));
+            var builtTaskGraphs = buildTasks.Select(buildTask => ToTaskGraph(stdout,
+                                                                             buildTask,
+                                                                             taskNumberAssigner,
+                                                                             buildStopwatch));
+            new TaskGraph(builtTaskGraphs).Run();
+        }
+
+        /// <summary>
+        ///   Executes the build described by the build tasks.
+        /// </summary>
+        /// <param name="buildTasks">the tasks that describe the build.</param>
+        public static void RunBuild(params BuildTask[] buildTasks) => RunBuild(Console.Out, buildTasks);
+
+        /// <summary>
+        ///   Creates a build task that will invoke the given action.
+        /// </summary>
+        /// <param name="action">the action to invoke in this build task.</param>
+        /// <param name="name">
+        ///   the name of this build task (this name will be used in the build output and build logs).
+        /// </param>
+        /// <param name="dependsOn">other build tasks that should be invoked before this build task.</param>
+        /// <returns>the build task object.</returns>
+        public static BuildTask Build(BuildAction action,
+                                      string name = null,
+                                      IEnumerable<BuildTask> dependsOn = null)
+            => new BuildTask(action, name, dependsOn);
+
+        private static int CountTasks(IReadOnlyCollection<BuildTask> buildTasks)
+            => buildTasks.Count + buildTasks.Select(task => CountTasks(task.Dependencies)).Sum();
+
+        private static TaskGraph ToTaskGraph(TextWriter stdout,
+                                             BuildTask buildTask,
+                                             TaskNumberAssigner taskNumberAssigner,
+                                             Stopwatch buildStopwatch) {
+            var taskGraphs = buildTask.Dependencies
+                                      .Select(dependencyBuildTask => ToTaskGraph(stdout,
+                                                                                 dependencyBuildTask,
+                                                                                 taskNumberAssigner,
+                                                                                 buildStopwatch))
+                                      .ToImmutableArray();
+            var thisTaskNumber = taskNumberAssigner.AssignNumber();
+            return new TaskGraph(
+                () => {
+                    LogBuildStart(stdout, buildTask, taskNumberAssigner, buildStopwatch, thisTaskNumber);
+                    buildTask.Action(new BuildActionContext());
+                    LogBuildEnd(stdout, buildTask, taskNumberAssigner, buildStopwatch, thisTaskNumber);
+                },
+                taskGraphs);
+        }
+
+        private static void LogBuildStart(TextWriter stdout,
+                                          BuildTask buildTask,
+                                          TaskNumberAssigner taskNumberAssigner,
+                                          Stopwatch buildStopwatch,
+                                          int thisTaskNumber) {
+            var message = string.IsNullOrEmpty(buildTask.Name) ?
+                              "Started building." :
+                              $"Started building '{buildTask.Name}'.";
+            WriteLogLine(stdout, thisTaskNumber, buildStopwatch, message, taskNumberAssigner.TotalTasks);
+        }
+
+        private static void LogBuildEnd(TextWriter stdout,
+                                        BuildTask buildTask,
+                                        TaskNumberAssigner taskNumberAssigner,
+                                        Stopwatch buildStopwatch,
+                                        int thisTaskNumber) {
+            var message = string.IsNullOrEmpty(buildTask.Name) ?
+                              "Done building." :
+                              $"Done building '{buildTask.Name}'.";
+            WriteLogLine(stdout, thisTaskNumber, buildStopwatch, message, taskNumberAssigner.TotalTasks);
+        }
+
+        private static void WriteLogLine(TextWriter logWriter,
+                                         int taskNumber,
+                                         Stopwatch stopwatch,
+                                         string msg,
+                                         int totalTasks)
+            => logWriter.WriteLine("[{0}/{1} {2}s] {3}", taskNumber, totalTasks, GetTimestamp(stopwatch), msg);
+
+        private static string GetTimestamp(Stopwatch buildStopwatch)
+            => ((double) buildStopwatch.ElapsedMilliseconds / 1000).ToString("F3")
+                                                                   .PadLeft(ReservedTimeStringLength);
     }
 
-    /// <summary>
-    ///   Executes the build described by the build tasks.
-    /// </summary>
-    /// <param name="buildTasks">the tasks that describe the build.</param>
-    public static void RunBuild(params BuildTask[] buildTasks) => RunBuild(Console.Out, buildTasks);
+    internal class TaskNumberAssigner {
+        private int lastAssignedNumber;
 
-    /// <summary>
-    ///   Creates a build task that will invoke the given action.
-    /// </summary>
-    /// <param name="action">the action to invoke in this build task.</param>
-    /// <param name="name">
-    ///   the name of this build task (this name will be used in the build output and build logs).
-    /// </param>
-    /// <param name="dependsOn">other build tasks that should be invoked before this build task.</param>
-    /// <returns>the build task object.</returns>
-    public static BuildTask Build(BuildAction action, string name = null, IEnumerable<BuildTask> dependsOn = null)
-      => new BuildTask(action, name, dependsOn);
+        public TaskNumberAssigner(int totalTasks) {
+            TotalTasks = totalTasks;
+        }
 
-    private static int CountTasks(IReadOnlyCollection<BuildTask> buildTasks)
-      => buildTasks.Count + buildTasks.Select(task => CountTasks(task.Dependencies)).Sum();
+        public int TotalTasks { get; }
 
-    private static TaskGraph ToTaskGraph(TextWriter stdout, BuildTask buildTask, TaskNumberAssigner taskNumberAssigner, Stopwatch buildStopwatch) {
-      var taskGraphs = buildTask.Dependencies
-                                .Select(dependencyBuildTask => ToTaskGraph(stdout, dependencyBuildTask, taskNumberAssigner, buildStopwatch))
-                                .ToImmutableArray();
-      var thisTaskNumber = taskNumberAssigner.AssignNumber();
-      return new TaskGraph(
-        () => {
-          LogBuildStart(stdout, buildTask, taskNumberAssigner, buildStopwatch, thisTaskNumber);
-          buildTask.Action(new BuildActionContext());
-          LogBuildEnd(stdout, buildTask, taskNumberAssigner, buildStopwatch, thisTaskNumber);
-        },
-        taskGraphs);
+        public int AssignNumber() => ++lastAssignedNumber;
     }
-
-    private static void LogBuildStart(TextWriter stdout, BuildTask buildTask, TaskNumberAssigner taskNumberAssigner, Stopwatch buildStopwatch, int thisTaskNumber) {
-      var buildStartMessage = string.IsNullOrEmpty(buildTask.Name) ? "Started building." : $"Started building '{buildTask.Name}'.";
-      WriteLogLine(stdout, thisTaskNumber, buildStopwatch, buildStartMessage, taskNumberAssigner.TotalTasks);
-    }
-
-    private static void LogBuildEnd(TextWriter stdout, BuildTask buildTask, TaskNumberAssigner taskNumberAssigner, Stopwatch buildStopwatch, int thisTaskNumber) {
-      var buildDoneMessage = string.IsNullOrEmpty(buildTask.Name) ? "Done building." : $"Done building '{buildTask.Name}'.";
-      WriteLogLine(stdout, thisTaskNumber, buildStopwatch, buildDoneMessage, taskNumberAssigner.TotalTasks);
-    }
-
-    private static void WriteLogLine(TextWriter logWriter, int taskNumber, Stopwatch stopwatch, string msg, int totalTasks)
-      => logWriter.WriteLine("[{0}/{1} {2}s] {3}", taskNumber, totalTasks, GetTimestamp(stopwatch), msg);
-
-    private static string GetTimestamp(Stopwatch buildStopwatch)
-      => ((double) buildStopwatch.ElapsedMilliseconds/1000).ToString("F3").PadLeft(ReservedTimeStringLength);
-  }
-
-  internal class TaskNumberAssigner {
-    private int lastAssignedNumber;
-
-    public TaskNumberAssigner(int totalTasks) {
-      TotalTasks = totalTasks;
-    }
-
-    public int TotalTasks { get; }
-
-    public int AssignNumber() => ++lastAssignedNumber;
-  }
 }
