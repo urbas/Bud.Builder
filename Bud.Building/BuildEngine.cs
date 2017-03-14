14a9fd3a99311be82309928a14afe0011d68d2e0
Matej Urbas <matej.urbas@gmail.com>
2017-03-14 22:46:09 +0000

    Simplifies the build engine.

--- Bud.Building/BuildEngine.cs
+++ Bud.Building/BuildEngine.cs
@@ -37,6 +37,31 @@ namespace Bud {
   ///   into the output directory.
   /// </remarks>
   public class BuildEngine {
+    public string SourceDir { get; }
+    public string OutputDir { get; }
+    public string MetaDir { get; }
+    public string DoneOutputsDir { get; }
+    public string PartialOutputsDir { get; }
+
+    /// <summary>
+    ///   Task graphs are built up in a single thread. This is why this can be a normal dictionary.
+    /// </summary>
+    private readonly Dictionary<IBuildTask, TaskGraph> buildTaskToTaskGraph = new Dictionary<IBuildTask, TaskGraph>();
+
+    private readonly ConcurrentDictionary<IBuildTask, BuildTaskResult> buildTasksToResults
+      = new ConcurrentDictionary<IBuildTask, BuildTaskResult>(new Dictionary<IBuildTask, BuildTaskResult>());
+
+    private readonly ConcurrentDictionary<string, IBuildTask> signatureToBuildTask
+      = new ConcurrentDictionary<string, IBuildTask>();
+
+    private BuildEngine(string sourceDir, string outputDir, string metaDir) {
+      SourceDir = sourceDir;
+      OutputDir = outputDir;
+      MetaDir = metaDir;
+      PartialOutputsDir = Combine(MetaDir, ".partial");
+      DoneOutputsDir = Combine(MetaDir, ".done");
+    }
+
     ///  <summary>
     ///  </summary>
     /// <param name="sourceDir">the directory in which all the sources relevant to the build reside.</param>
@@ -63,84 +88,35 @@ namespace Bud {
     ///  <returns>an object containing information about the resulting build.</returns>
     ///  <exception cref="Exception">this exception is thrown if the build fails for any reason.</exception>
     public static void Execute(string sourceDir, string outputDir, string metaDir, IEnumerable<IBuildTask> buildTasks) {
-      var buildExecutionContext = new BuildExecutionContext(sourceDir, outputDir, metaDir);
-      ExecuteBuildTasks(buildTasks, buildExecutionContext);
-      AssertNoClashingFiles(buildExecutionContext);
-      CopyOutputToOutputDir(buildExecutionContext);
+      new BuildEngine(sourceDir, outputDir, metaDir).Execute(buildTasks);
     }
 
-    private static void ExecuteBuildTasks(IEnumerable<IBuildTask> buildTasks, BuildExecutionContext buildExecutionContext) {
+    private void Execute(IEnumerable<IBuildTask> buildTasks) {
+      CreateMetaOutputDirs();
+      ExecuteBuildTasks(buildTasks);
+      AssertNoClashingFiles();
+      CopyOutputToOutputDir();
+    }
+
+    private void CreateMetaOutputDirs() {
+      CreateDirectory(DoneOutputsDir);
+      CreateDirectory(PartialOutputsDir);
+    }
+
+    private void ExecuteBuildTasks(IEnumerable<IBuildTask> buildTasks) {
       try {
-        new TaskGraph(buildTasks.Select(buildTask => GetOrCreateTaskGraph(buildExecutionContext, buildTask))).Run();
+        new TaskGraph(buildTasks.Select(GetOrCreateTaskGraph)).Run();
       } catch (AggregateException aggregateException) {
         throw aggregateException.InnerExceptions[0];
       }
     }
 
-    private static void CopyOutputToOutputDir(BuildExecutionContext buildExecutionContext) {
-      if (Exists(buildExecutionContext.BuildDir)) {
-        Delete(buildExecutionContext.BuildDir, true);
-      }
-      foreach (var taskOutputDir in buildExecutionContext.TaskOutputDirs) {
-        CopyTree(taskOutputDir, buildExecutionContext.BuildDir);
-      }
-    }
-
-    private static TaskGraph GetOrCreateTaskGraph(BuildExecutionContext buildExecutionContext, IBuildTask buildTask) {
-      TaskGraph taskGraph;
-      if (buildExecutionContext.TryGetTaskGraph(buildTask, out taskGraph)) {
-        return taskGraph;
-      }
-      var createdTaskGraph = CreateTaskGraph(buildExecutionContext, buildTask);
-      buildExecutionContext.AddTaskGraph(buildTask, createdTaskGraph);
-      return createdTaskGraph;
-    }
-
-    private static TaskGraph CreateTaskGraph(BuildExecutionContext buildExecutionContext, IBuildTask buildTask)
-      => ToTaskGraph(buildExecutionContext,
-                     buildTask,
-                     buildTask.Dependencies
-                              .Select(depTask => GetOrCreateTaskGraph(buildExecutionContext, depTask))
-                              .ToImmutableArray());
-
-    private static TaskGraph ToTaskGraph(BuildExecutionContext buildExecutionContext, IBuildTask buildTask,
-                                         ImmutableArray<TaskGraph> dependenciesTaskGraphs)
-      => new TaskGraph(() => {
-        // At this point all dependencies will have been evaluated, so their results will be available.
-        var dependenciesResults = buildTask.Dependencies
-                                           .Select(buildExecutionContext.GetBuildTaskResult)
-                                           .ToImmutableArray();
-
-        var taskSignature = buildTask.GetSignature(buildExecutionContext.SourceDir, dependenciesResults);
-
-        AssertUniqueSignature(buildExecutionContext, buildTask, taskSignature);
-
-        var taskOutputDir = Combine(buildExecutionContext.DoneOutputsDir, taskSignature);
-        if (!Exists(taskOutputDir)) {
-          var partialTaskOutputDir = Combine(buildExecutionContext.PartialOutputsDir, taskSignature);
-          ExecuteBuildTask(buildTask, partialTaskOutputDir, taskOutputDir, buildExecutionContext.SourceDir,
-          dependenciesResults);
-        }
-
-        var buildTaskResult = new BuildTaskResult(buildTask, taskSignature, taskOutputDir, dependenciesResults);
-
-        buildExecutionContext.AddBuildTaskResult(buildTask, buildTaskResult);
-      }, dependenciesTaskGraphs);
-
-    private static void ExecuteBuildTask(IBuildTask buildTask, string partialTaskOutputDir, string taskOutputDir,
-                                         string sourceDir, ImmutableArray<BuildTaskResult> dependenciesResults) {
-      CreateDirectory(partialTaskOutputDir);
-      buildTask.Execute(new BuildTaskContext(partialTaskOutputDir, sourceDir), dependenciesResults);
-      Move(partialTaskOutputDir, taskOutputDir);
-    }
-
-    private static void AssertNoClashingFiles(BuildExecutionContext buildExecutionContext) {
+    private void AssertNoClashingFiles() {
       var relativeOutputFileToBuildTask = new Dictionary<string, IBuildTask>();
-      foreach (var signatureAndBuildTask in buildExecutionContext.SignaturesToBuildTasks) {
-        var relativeOutputFilesEnumerable =
-          FindFilesRelative(Combine(buildExecutionContext.DoneOutputsDir, signatureAndBuildTask.Key));
+      foreach (var signatureAndBuildTask in signatureToBuildTask) {
+        var relativeOutputFiles = FindFilesRelative(Combine(DoneOutputsDir, signatureAndBuildTask.Key));
 
-        foreach (var relativeOutputFile in relativeOutputFilesEnumerable) {
+        foreach (var relativeOutputFile in relativeOutputFiles) {
           IBuildTask otherTask;
 
           if (relativeOutputFileToBuildTask.TryGetValue(relativeOutputFile, out otherTask)) {
@@ -152,65 +128,67 @@ namespace Bud {
       }
     }
 
-    private static void AssertUniqueSignature(BuildExecutionContext buildExecutionContext, IBuildTask buildTask, string taskSignature) {
-      var storedTask = buildExecutionContext.GetOrAddTaskSignature(taskSignature, buildTask);
+    private void CopyOutputToOutputDir() {
+      if (Exists(OutputDir)) {
+        Delete(OutputDir, true);
+      }
+      foreach (var taskOutputDir in TaskOutputDirs) {
+        CopyTree(taskOutputDir, OutputDir);
+      }
+    }
+
+    private TaskGraph GetOrCreateTaskGraph(IBuildTask buildTask) {
+      TaskGraph taskGraph;
+      if (buildTaskToTaskGraph.TryGetValue(buildTask, out taskGraph)) {
+        return taskGraph;
+      }
+      var createdTaskGraph = CreateTaskGraph(buildTask);
+      buildTaskToTaskGraph.Add(buildTask, createdTaskGraph);
+      return createdTaskGraph;
+    }
+
+    private IEnumerable<string> TaskOutputDirs
+      => signatureToBuildTask.Keys.Select(sig => Combine(DoneOutputsDir, sig));
+
+    private TaskGraph CreateTaskGraph(IBuildTask buildTask)
+      => ToTaskGraph(buildTask, buildTask.Dependencies.Select(GetOrCreateTaskGraph).ToImmutableArray());
+
+    private TaskGraph ToTaskGraph(IBuildTask buildTask, ImmutableArray<TaskGraph> dependenciesTaskGraphs)
+      => new TaskGraph(() => {
+        // At this point all dependencies will have been evaluated, so their results will be available.
+        var dependenciesResults = buildTask.Dependencies
+                                           .Select(task => buildTasksToResults[task])
+                                           .ToImmutableArray();
+
+        var taskSignature = buildTask.GetSignature(SourceDir, dependenciesResults);
+
+        AssertUniqueSignature(buildTask, taskSignature);
+
+        var taskOutputDir = Combine(DoneOutputsDir, taskSignature);
+        if (!Exists(taskOutputDir)) {
+          var partialTaskOutputDir = Combine(PartialOutputsDir, taskSignature);
+          ExecuteBuildTask(buildTask, partialTaskOutputDir, taskOutputDir, SourceDir,
+          dependenciesResults);
+        }
+
+        var buildTaskResult = new BuildTaskResult(buildTask, taskSignature, taskOutputDir, dependenciesResults);
+
+        buildTasksToResults.TryAdd(buildTask, buildTaskResult);
+      }, dependenciesTaskGraphs);
+
+    private static void ExecuteBuildTask(IBuildTask buildTask, string partialTaskOutputDir, string taskOutputDir,
+                                         string sourceDir, ImmutableArray<BuildTaskResult> dependenciesResults) {
+      CreateDirectory(partialTaskOutputDir);
+      buildTask.Execute(new BuildTaskContext(partialTaskOutputDir, sourceDir), dependenciesResults);
+      Move(partialTaskOutputDir, taskOutputDir);
+    }
+
+    private void AssertUniqueSignature(IBuildTask buildTask, string taskSignature) {
+      var storedTask = signatureToBuildTask.GetOrAdd(taskSignature, buildTask);
       if (storedTask != buildTask) {
         throw new Exception($"Tasks '{storedTask.Name}' and '{buildTask.Name}' are clashing. " +
                             $"They have the same signature '{taskSignature}'.");
       }
-    }
-
-    private class BuildExecutionContext {
-      /// <summary>
-      ///   Task graphs are built up in a single thread. This is why this can be a normal dictionary.
-      /// </summary>
-      private readonly Dictionary<IBuildTask, TaskGraph> buildTaskToTaskGraph = new Dictionary<IBuildTask, TaskGraph>();
-
-      private readonly ConcurrentDictionary<IBuildTask, BuildTaskResult> buildTasksToResults
-        = new ConcurrentDictionary<IBuildTask, BuildTaskResult>(new Dictionary<IBuildTask, BuildTaskResult>());
-
-      private readonly ConcurrentDictionary<string, IBuildTask> signatureToBuildTask
-        = new ConcurrentDictionary<string, IBuildTask>();
-
-      public BuildExecutionContext(string sourceDir, string buildDir, string metaDir) {
-        SourceDir = sourceDir;
-        BuildDir = buildDir;
-        MetaDir = metaDir;
-        PartialOutputsDir = Combine(MetaDir, ".partial");
-        DoneOutputsDir = Combine(MetaDir, ".done");
-
-        CreateDirectory(DoneOutputsDir);
-        CreateDirectory(PartialOutputsDir);
-      }
-
-      public string SourceDir { get; }
-
-      public string BuildDir { get; }
-
-      private string MetaDir { get; }
-
-      public string DoneOutputsDir { get; }
-
-      public string PartialOutputsDir { get; }
-
-      public IEnumerable<string> TaskOutputDirs
-        => signatureToBuildTask.Keys.Select(sig => Combine(DoneOutputsDir, sig));
-
-      public IDictionary<string, IBuildTask> SignaturesToBuildTasks => signatureToBuildTask;
-
-      public void AddBuildTaskResult(IBuildTask buildTask, BuildTaskResult buildTaskResult)
-        => buildTasksToResults.GetOrAdd(buildTask, buildTaskResult);
-
-      public BuildTaskResult GetBuildTaskResult(IBuildTask buildTask) => buildTasksToResults[buildTask];
-
-      public bool TryGetTaskGraph(IBuildTask buildTask, out TaskGraph taskGraph)
-        => buildTaskToTaskGraph.TryGetValue(buildTask, out taskGraph);
-
-      public void AddTaskGraph(IBuildTask buildTask, TaskGraph taskGraph)
-        => buildTaskToTaskGraph.Add(buildTask, taskGraph);
-
-      public IBuildTask GetOrAddTaskSignature(string taskSignature, IBuildTask buildTask)
-        => signatureToBuildTask.GetOrAdd(taskSignature, buildTask);
     }
   }
 
